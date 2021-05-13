aef875cfc1671d9172c491761dd5113718e84267
soso03185 <34054994+soso03185@users.noreply.github.com>
2021-05-14 01:38:59 +0900

    .

---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager
+{
+    public void Play(Define.Sound type, string path, float pitch = 1.0f)
+    {
+
+    }
+
+
+}
