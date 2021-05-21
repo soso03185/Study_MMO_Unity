b9d21007aa39cd116ed27cf2e67d002ac70a44fc
soso03185 <34054994+soso03185@users.noreply.github.com>
2021-05-22 01:27:28 +0900

    Data Manager

--- Assets/Scripts/Managers/DataManager.cs
+++ Assets/Scripts/Managers/DataManager.cs
@@ -1,16 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DataManager : MonoBehaviour
+public interface ILoader<Key, Value>
 {
-    void Start()
+    Dictionary<Key, Value> MakeDict();
+}
+
+public class DataManager
+{
+    public Dictionary<int, Stat> StatDict { get; private set; } = new Dictionary<int, Stat> ();
+
+    public void Init()
     {
-        
+        StatDict = LoadJson<StatData, int, Stat>("StatData").MakeDict();
     }
 
-    void Update()
+    Loader LoadJson<Loader, Key, Value>(string path) where Loader : ILoader<Key, Value>
     {
-        
+        TextAsset textAsset = Managers.Resource.Load<TextAsset>($"Data/{path}");
+        return JsonUtility.FromJson<Loader>(textAsset.text);
     }
 }
